616681a0a4caa44a2b5e65eaefb7e82043fd0fd6
Selman <selman.ciftci.coding@gmail.com>
2023-12-14 17:58:06 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <div className="flex flex-col gap-10 justify-center items-center text-white my-4">
+        <h1 className="text-5xl">Template: React + TypeScript + Tailwind</h1>
+        <p className="w-[70ch] text-3xl">
+          This template is powered by Vite, a fast and opinionated frontend
+          build tool. Leveraging the efficiency of React for dynamic UIs, the
+          robustness of TypeScript for static typing, and the utility-first
+          approach of Tailwind CSS for styling, this combination crafted a
+          modern and streamlined development environment.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
